c8f0df35d55f25f1a90390b312f67e8481fade68
Lardeck <martinw1394@gmail.com>
2021-02-18 22:04:10 +0100

    increase version

--- MartinsAltManager.lua
+++ MartinsAltManager.lua
@@ -35,7 +35,7 @@ local min_x_size = 360;
 local min_level = GetMaxLevelForExpansionLevel(GetExpansionLevel());
 local locale = GetLocale()
 
-local VERSION = "9.0.16.7"					   
+local VERSION = "9.0.16.8"
 
 local defaultDB = {
     profile = {
