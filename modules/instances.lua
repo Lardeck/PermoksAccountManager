8cadb1703adfde1117efcb78c5f55f110f6a0d31
Lardeck <martinw1394@gmail.com>
2021-03-08 14:57:39 +0100

    fix raid tooltip sorting

--- modules/instances.lua
+++ modules/instances.lua
@@ -102,7 +102,7 @@ function AltManager:RaidTooltip_OnEnter(button, alt_data, name)
 	tooltip:AddLine("")
 
 	--spairs(db.data, function(t, a, b)  return t[a].ilevel > t[b].ilevel end) do
-	for difficulty, info in AltManager.spairs(raidInfo, function(t, a, b) if a == 17 then return b < a else return a < b end end) do
+	for difficulty, info in AltManager.spairs(raidInfo, function(t, a, b) if a and b then if a<17 and b<17 then return a < b else return a > b end end end) do
 		tooltip:AddLine(info.difficulty..":", "", self:CreateQuestString(info.defeatedEncounters, info.numEncounters))
 	end
 
