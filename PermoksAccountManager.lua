7711e2507ba14106a00bed9a77f3a8df00ccfa1c
Permok <martinw1394@gmail.com>
2023-11-12 14:40:19 +0100

    Bump version

--- PermoksAccountManager.lua
+++ PermoksAccountManager.lua
@@ -38,7 +38,7 @@ local LibQTip = LibStub('LibQTip-1.0')
 local L = LibStub('AceLocale-3.0'):GetLocale(addonName)
 local LSM = LibStub('LibSharedMedia-3.0')
 local VERSION = '1.1.23'
-local INTERNALVERSION = 29
+local INTERNALVERSION = 30
 local INTERNALWOTLKVERSION = 6
 local defaultDB = {
     profile = {
@@ -722,6 +722,11 @@ function PermoksAccountManager:Modernize(oldInternalVersion)
     if oldInternalVersion < 29 then
         self:AddLabelToDefaultCategory('general', 'dream_infusion')
         self:AddLabelToDefaultCategory('currentweekly', 'sparks_of_life')
+    end
+
+    if oldInternalVersion < 30 then
+        self:AddLabelToDefaultCategory('currentweekly', 'dreamsurge_weekly', 13)
+        self:AddLabelToDefaultCategory('currentweekly', 'time_rift', 14)
     end
 end
 
